b755b87b9787fbe91f3edc56bcc54c2fe5393e8e
Ronald Nijssen <hed320@hotmail.com>
2016-04-07 14:49:48 +0200

    webshop

--- php/registratie.php
+++ php/registratie.php
@@ -1,3 +1,9 @@
 <?php
 $content = new TemplatePower("html/registratie.html");
 $content->prepare();
+
+if (!empty($_POST["voornaam"]) and !empty($_POST["achternaam"]) and !empty($_POST["email"]) and !empty($_POST["wachtwoord"]) and !empty($_POST["wachtwoord"])) {
+
+} else {
+    $content->newBlock("FORMULIER");
+}
--- php/webshop.php
+++ php/webshop.php
@@ -1,3 +1,6 @@
 <?php
 $content = new TemplatePower("html/webshop.html");
 $content->prepare();
+
+$content->newBlock("PRODUCT");
+$content->assign("PRODUCTID", 1);
